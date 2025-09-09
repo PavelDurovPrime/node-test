9d039559d92030c02fc04209df08882823c5923c
Vulture <screamzyaka@gmail.com>
2025-09-09 12:07:46 +0300

    Lab 1 : scalar types and references, type counting

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log('hello GitHub');
+function inc(n) {
+    return n+1
+
+}
+
+const a = 5;
+
+const b  = inc(a);
+
+console.dir({a, b});
+
+function incobj(num) {
+    num.n = num.n + 1;
+}
+
+const obj = {n: 5};
+incobj(obj);
+console.dir({obj});
+
+
+const arr = [true, 'hello',5, 12,-200 , false , 'word', 99, 'JS', true ];
+
+const typesCount = {};
+
+for(const item of arr) {
+    const type = typeof item ; 
+if (typesCount[type]) {
+    typesCount[type]++;
+} else {
+    typesCount[type] = 1;   
+}
+}
+
+const arr2= [true, 'hello',5,12, -200, false, false, 'word', null, undefined, {x:1}]
+
+
+
+for(const item of arr) {
+    const type = typeof item ;
+    if (typesCount[type]) {
+        typesCount[type]++;
+    } else {
+        typesCount[type] = 1;
+    }   
+}
+console.log(typesCount);
+
+
